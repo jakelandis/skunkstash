5573c7e91a7b2296229125c3ca8189e62cd78600
Jake Landis <jake.landis@elastic.co>
2017-08-21 08:39:36 -0500

    add singleton as option in the annotation

--- api/src/main/java/org/logstash/skunk/api/plugin/LogStashPlugin.java
+++ api/src/main/java/org/logstash/skunk/api/plugin/LogStashPlugin.java
@@ -6,5 +6,13 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface LogStashPlugin {
+    /**
+     * The name of the plugin
+     */
     String value();
+
+    /**
+     * Declares if there should be at most 1 single instance. Plugins that declare this to be true MUST be thread safe.
+     */
+    boolean singleton() default false;
 }
